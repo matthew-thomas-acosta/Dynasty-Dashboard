2a97b57414954d31c6e4d3d3621d397659412338
mattacostadev <71569483+mattacostadev@users.noreply.github.com>
2021-01-16 16:25:28 -0700

    sportsipy setup

---
+++ src/setup_data.py
@@ -0,0 +1,5 @@
+import sportsipy.nfl.player as plyr
+import sportsipy.nfl.teams as tms
+import sportsipy.nfl.roster as rstr
+import sportsipy.nfl.boxscore as bx
+import sportsipy.nfl.schedule as scdl
