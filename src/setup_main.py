4b5d3bed72cf494082c3d02c46e3759820a946d8
mattacostadev <71569483+mattacostadev@users.noreply.github.com>
2021-01-16 11:43:42 -0700

    added package_espn_receiving_data

--- src/setup_main.py
+++ src/setup_main.py
@@ -7,8 +7,17 @@ espn_fantasy_teams = setup_ESPN.teams
 
 
 def print_espn_player(player):
-    for each in player.stats:
-        print(each)
+    print(player.name)
+    for i in range(18):
+        if i in player.stats:
+            for key in player.stats[i]:
+                if key == 'breakdown':
+                    for stat_key in player.stats[i][key]:
+                        print('{} -> {}'.format(stat_key, player.stats[i][key][stat_key]))
+                elif 'projected' in key:
+                    continue
+                else:
+                    print('{} -> {}'.format(key, player.stats[i][key]))
 
 
 def find_player_owner(player):
@@ -37,6 +46,70 @@ def package_espn_player_data(player):
     espn_player_header['owned'] = not (espn_player_header['owner_id'] == 0)
     espn_player['header'] = espn_player_header
 
-    espn_player_stats = {}
+    espn_player_stats = {
+        'receiving': package_espn_receiving_data(player),
+        'rushing': package_espn_rushing_data(player),
+        'passing': package_espn_passing_data(player),
+        'def': package_espn_defense_data(player),
+        'kick': package_espn_kicking_data(player)
+    }
+
+    espn_player['stats'] = espn_player_stats
+
+    return espn_player
+
+
+def package_espn_receiving_data(player):
+    receiving_data = {}
+
+    for i in range(18):
+        if i in player.stats:
+            if 'breakdown' in player.stats[i]:
+                rec_stats_recorded = False
+
+                for key in player.stats[i]['breakdown']:
+                    if 'receiving' in key:
+                        rec_stats_recorded = True
+
+                if rec_stats_recorded:
+                    receiving_data[i] = {
+                        'rec_receptions': 0,
+                        'rec_yards': 0,
+                        'rec_touchdowns': 0,
+                        'rec_2pt_conversions': 0
+                    }
+                else:
+                    receiving_data[i] = None
+
+                for key in player.stats[i]['breakdown']:
+                    if key == 'receivingReceptions':
+                        receiving_data[i]['rec_receptions'] = player.stats[i]['breakdown'][key]
+                    elif key == 'receivingYards':
+                        receiving_data[i]['rec_yards'] = player.stats[i]['breakdown'][key]
+                    elif key == 'receivingTouchdowns':
+                        receiving_data[i]['rec_touchdowns'] = player.stats[i]['breakdown'][key]
+                    elif key == 'receiving2PtConversions':
+                        receiving_data[i]['rec_2pt_conversions'] = player.stats[i]['breakdown'][key]
+            else:
+                receiving_data[i] = None
+        else:
+            receiving_data[i] = None
+
+    return receiving_data
+
+
+def package_espn_rushing_data(player):
+    return ''
+
+
+def package_espn_passing_data(player):
+    return ''
+
+
+def package_espn_defense_data(player):
+    return ''
+
+def package_espn_kicking_data(player):
+    return ''
 
 
