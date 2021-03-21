cfb10169f051a11f72b541019a7640853e569ec8
mattacostadev <71569483+mattacostadev@users.noreply.github.com>
2021-03-21 04:48:10 -0700

    top level of parser

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ import setup_ESPN
 import setup_data as data
 
 if __name__ == '__main__':
-    data.active_player_urls()
+    data.parse_active_player_urls()
 
--- src/setup_data.py
+++ src/setup_data.py
@@ -4,10 +4,61 @@ import string
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\Matthew Acosta\PycharmProjects\DynastyDashboardDataCollection\chromedriver.exe')
+driver = webdriver\
+    .Chrome(executable_path=r'C:\Users\Matthew Acosta\PycharmProjects\DynastyDashboardDataCollection\chromedriver.exe')
 
 
-def active_player_urls():
+def parse_active_player_urls():
+    urls = get_active_player_urls()
+
+    active_player_stats = []
+
+    i = 1
+    for url in urls:
+        url_string = strip_url(url)
+
+        driver.get('https://www.pro-football-reference.com{}'.format(url_string))
+        content = driver.page_source
+        player_page = BeautifulSoup(content)
+
+        player = {}
+
+        player_header = parse_player_header(player_page.find(attrs={'class': 'players'}))
+        player['header'] = player_header
+
+        player_stats = {}
+
+        for attr in player_page.find_all(attrs={'class': 'table_container is_setup'}):
+            if attr['id'] == 'div_passing':
+                player_stats['passing'] = parse_passing(attr)
+            elif attr['id'] == 'div_rushing_and_receiving':
+                player_stats['rush_rec'] = parse_rushing_receiving(attr)
+            elif attr['id'] == 'div_defense':
+                player_stats['def'] = parse_defense(attr)
+
+        player['stats'] = player_stats
+        active_player_stats.append(player)
+
+        i = i + 1
+
+
+def parse_player_header(header):
+    return header
+
+
+def parse_passing(passing):
+    return passing
+
+
+def parse_rushing_receiving(rush_rec):
+    return rush_rec
+
+
+def parse_defense(defense):
+    return defense
+
+
+def get_active_player_urls():
     alphabet = list(string.ascii_uppercase)
 
     results = []
@@ -15,9 +66,9 @@ def active_player_urls():
     for ltr in alphabet:
         driver.get('https://www.pro-football-reference.com/players/{}/'.format(ltr))
         content = driver.page_source
-        soup = BeautifulSoup(content)
+        letter_page = BeautifulSoup(content)
 
-        for attr in soup.find_all(attrs={'class':'section_content'}):
+        for attr in letter_page.find_all(attrs={'class':'section_content'}):
             if attr['id'] == 'div_players':
                 names = attr.find_all('b')
                 for name in names:
@@ -25,4 +76,11 @@ def active_player_urls():
                         results.append(name)
 
     return results
+
+
+def strip_url(url):
+    url_string = str(url).replace('<b><a href="', '')
+    after_htm = url_string.index('"')
+    url_string = url_string[0:after_htm]
+    return url_string
 
