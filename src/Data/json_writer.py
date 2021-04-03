9aa64995539f8f816780514be825a72bb5dfb22f
mattacostadev <71569483+mattacostadev@users.noreply.github.com>
2021-04-03 14:26:06 -0700

    Add Django

--- src/Data/json_writer.py
+++ src/Data/json_writer.py
@@ -20,13 +20,67 @@ def connect(db_path):
     except Error as error:
         print(error)
 
-    write_espn_data(db_conn)
-    write_player_data(db_conn)
+    create_espn_tables(db_conn)
+    create_player_tables(db_conn)
 
 
-def write_espn_data(db_conn):
-    return ''
+def create_espn_tables(db_conn):
+    espn_team_table = """CREATE TABLE IF NOT EXISTS tblESPNTeams (
+                            ID integer PRIMARY KEY,
+                            name text NOT NULL,
+                            owner text NOT NULL,
+                            wins integer NOT NULL,
+                            losses integer NOT NULL,
+                            ties integer NOT NULL,
+                            pts_for float NOT NULL,
+                            pts_against float NOT NULL
+                        );"""
 
+    espn_scoring_table = """CREATE TABLE IF NOT EXISTS tblESPNScoring (
+                            ID integer PRIMARY KEY,
+                            category text NOT NULL,
+                            item text NOT NULL,
+                            value float NOT NULL
+                        );"""
 
-def write_player_data(db_conn):
+    if db_conn is not None:
+        create_table(db_conn, espn_team_table)
+        create_table(db_conn, espn_scoring_table)
+        print('ESPN tables created')
+    else:
+        print('ERROR: no db conn')
+
     return
+
+
+def create_player_tables(db_conn):
+    player_table = """CREATE TABLE IF NOT EXISTS tblPlayers (
+                            ID integer PRIMARY KEY,
+                            PFR_ID text NOT NULL,
+                            position text NOT NULL,
+                            team text NOT NULL,
+                            college text,
+                            age integer NOT NULL,
+                            FOREIGN KEY (espn_team) REFERENCES tblESPNTeams (ID)
+                        );"""
+
+    player_season_table = """CREATE TABLE IF NOT EXISTS tblPlayerSeasons (
+                            ID integer PRIMARY KEY
+                        );"""
+
+    if db_conn is not None:
+        create_table(db_conn, player_table)
+        create_table(db_conn, player_season_table)
+        print('data tables created')
+    else:
+        print('ERROR: no db conn')
+
+    return
+
+
+def create_table(db_conn, table):
+    try:
+        cursor = db_conn.cursor()
+        cursor.execute(table)
+    except Error as error:
+        print(error)
--- src/main.py
+++ src/main.py
@@ -5,4 +5,3 @@ from src.Data import json_writer as jsn
 if __name__ == '__main__':
     setup.collect_data()
     jsn.connect(jsn.path)
-
